0f0384829718a530b7e94e7cefada7b73e9b8a06
William Taylor <william.taylor@fireeye.com>
2018-07-20 14:13:39 -0400

    Initial commit for crypto price querying support

---
+++ cryptoprice.py
@@ -0,0 +1,49 @@
+import difflib
+import requests
+
+def get_price(name):
+    name, ticket = get_key_value(name, cryptos)
+    if not name:
+        raise KeyError('currency not found')
+
+    api_link = "https://min-api.cryptocompare.com/data/price?fsym={0}&tsyms=USD".format(ticker)
+    api_response = requests.get(api_link)
+    price = api_response.json().get("USD", 'Unavailable')
+
+    return price
+
+def currency_supported(name):
+    cryptos = json_to_dictionary('data/cryptotickers.json')
+
+    crypto_name, crypto_symbol = get_key_and_value_match(name, cryptos)
+    if not crypto_name:
+        return False
+
+def get_key_value(key_word, dictionary):
+    """
+    Takes a key word and finds the key or value inside the dictionary which
+    matches it and returns the corresponding key and value pair. If there is
+    no direct match, the nearest key or value is then found.
+    Dictionary is expected to be with lower case keys and upper case values.
+    """
+    reverse_dictionary = {dictionary[k]: k for k in dictionary}
+    if key_word in dictionary:
+        key = key_word
+        value = dictionary[key_word]
+    elif key_word in reverse_dictionary:
+        key = reverse_dictionary[key_word]
+        value = key_word
+    else:
+        all_names = {k.lower(): k for k in dictionary.keys()}
+        all_names.update({v.lower(): v for v in dictionary.values()})
+        closest_guesses = difflib.get_close_matches(key_word.lower(), all_names)
+        if len(closest_guesses) > 0:
+            closest_guess = all_names[closest_guesses[0]]
+            if closest_guess in dictionary.keys():
+                key = closest_guess
+                value = dictionary[closest_guess]
+            elif closest_guess in reverse_dictionary.keys():
+                key = reverse_dictionary[closest_guess]
+                value = closest_guess
+
+    return key, value
